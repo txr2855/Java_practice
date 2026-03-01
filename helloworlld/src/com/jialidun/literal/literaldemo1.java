bad5dcdbf2352875d4643b80f52994ea70eca015
txr2855 <1778947680@qq.com>
2026-03-01 15:44:00 +0800

    3.1

---
+++ helloworlld/src/com/jialidun/literal/literaldemo1.java
@@ -0,0 +1,17 @@
+package com.jialidun.literal;
+
+public class literaldemo1 {
+    public static void main(String[] args) {
+        //输出我的身高180
+        System.out.println("我的身高180");
+        //输出我的体重100t
+        System.out.println("我的体重100t");
+        //输出我的年龄18
+        System.out.println("我的年龄18");
+        //输出我的性别男
+        System.out.println("我的性别男");
+        System.out.println("我的爱好无");
+    }
+
+}
+//
--- helloworlld/src/com/jialidun/test/helloworld.java
+++ helloworlld/src/com/jialidun/test/helloworld.java
@@ -1,7 +1,32 @@
 package com.jialidun.test;
+//表示现在的类定义在哪个包下，比如这个就是在com.jialidun.test包下
+//不用自己写自动生成
 
+
+
+//表示定义一个类
+//class
+//helloworld：类名
+//{}：表示类的范围；所有的代码都写在这个大括号中
 public class helloworld {
+
+
+    //表示java程序的主入口，当程序开始运行的时候，会从主入口开始逐行往下执行
     public static void main(String[] args) {
+        //表示输出语句。输出语句的格式是System.out.println()
         System.out.println("hello world");
+        System.out.println("1111");
+        System.out.println("2222");
     }
 }
+//1.1注释
+// 方法:写//，多行注释是/* */
+/*
+注释的快捷键ctrl+/
+多行是：先选中文本再ctrl+shift+/
+注释不会影响运行
+不要嵌套注释
+ */
+//1.2关键字
+//特点：字母全部小写，单词高亮显示
+//class创建一个类，或叫定义一个类，
